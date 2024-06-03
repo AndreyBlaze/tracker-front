b70198856624ae0fae82fb40b29f39f8bf263592
andrey <feand20041102@gmail.com>
2024-06-04 01:05:22 +0300

    up

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'projects', component: ProjectsComponent},
+  {path: 'projects/:id/dashboard', component: DashboardComponent},
+  {path: 'projects/:id/chats/:chatId', loadChildren: () => import('./pages/chat/chat.module').then(x => x.ChatModule)},
+  {path: 'chats/:id', loadChildren: () => import('./pages/chat/chat.module').then(x => x.ChatModule)},
   {path: '**', redirectTo: 'login'}
 ];
